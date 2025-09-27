c4e24eb6b68dc25856a2f3c22ddd9acf7ed13787
Garth <thaabit@gmail.com>
2025-09-27 03:56:12 -0600

    new build

--- vuejs/vite-project/src/App.vue
+++ vuejs/vite-project/src/App.vue
@@ -1,8 +1,14 @@
+<script setup>
+    import { useAuthStore } from '@/stores/auth.store.js';
+    const { logout } = useAuthStore();
+
+</script>
 <template>
     <RouterLink to="/">Home</RouterLink> |
     <RouterLink to="/signup">Signup</RouterLink> |
     <RouterLink to="/login">Login</RouterLink> |
     <RouterLink to="/games">Games</RouterLink> |
     <RouterLink to="/friends">Friends</RouterLink> |
+    <button @click="logout">Logout</button>
     <RouterView />
 </template>
