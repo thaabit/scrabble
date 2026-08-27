bca6d33455a60b90ec1fdbed159ab60174dd9cd3
Garth Hill <thaabit@gmail.com>
2026-08-27 11:36:06 +0000

    adding install script

---
+++ api/install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+sudo apt update
+sudo apt remove docker docker-engine docker.io containerd runc
+sudo apt install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run hello-world
